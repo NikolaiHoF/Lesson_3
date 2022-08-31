ff65b66a9a404089c774e89f72093f627664faa4
NikolaiHoF <Hof1000@gmail.com>
2022-09-01 01:07:50 +0700

    Добавил ДЗ №19, №21, №23

---
+++ Task_19/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+try
+{
+    Console.Write("Введите пятизначное число: ");
+    int palind = Int32.Parse(Console.ReadLine());
+    int num = palind;
+    int res = 0;
+
+    while (num > 0) 
+    {
+        res = res * 10;
+        res = res + num % 10;
+        num = num / 10;
+    }
+    if (palind == res)
+    {
+        Console.WriteLine($"Число {palind} является палиндромом.");
+    }
+    else
+    {
+        Console.WriteLine($"Число {palind} не является палиндромом.");
+    }
+}
+
+catch 
+{
+    Console.WriteLine("Вы ввели не числа или не пятизначное число");
+}
---
+++ Task_23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+string returnPOW()
+{
+    Console.WriteLine("Введите число, для составления таблицы кубов ");
+    int powNumb = Convert.ToInt32(Console.ReadLine());
+    int start = 1;
+    List<int> answer = new List<int> {};
+    while (start <= powNumb)
+    {
+        int numb = Convert.ToInt32(Math.Pow(start, 3));
+        start = start + 1;
+        answer.Add(numb);
+    }
+    string answer1 = string.Join(", ", answer.ToArray());
+    return answer1;
+}
+
+try
+{
+Console.WriteLine(returnPOW());
+}
+
+catch
+{
+Console.WriteLine("Вы ввели не число");  
+}
