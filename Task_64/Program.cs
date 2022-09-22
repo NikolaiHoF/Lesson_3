1c9a32bd9ad28e1e3c6bf41558a35172db6340f3
NikolaiHoF <hof1000@gmail.com>
2022-09-23 00:58:40 +0700

    ДЗ 64, 66, 68

---
+++ Task_64/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+string NaturalNumbers(int a, int n)
+{
+    if(a == n) return $"{n} ";
+    return NaturalNumbers(a, n - 1) + n + " ";
+}
+Console.WriteLine(NaturalNumbers(4, 8));
